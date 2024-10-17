4b2102601397739a874b65213ba1780ccbc30771
Tim Shichalin <shichatim@gmail.com>
2024-10-16 17:23:10 -0700

    user angle setting, rounding function, and no connection issues

--- ContentView.swift
+++ ContentView.swift
@@ -19,8 +19,11 @@ struct ContentView: View {
     @State private var wristCoords = CGPoint(x: 170.0, y: 100.0)
     
     // Round int to nearest 5 (thanks chat GPT)
-    func roundToNearestFive(_ number: Int) -> Int {
-        return 5 * Int(round(Double(number) / 5.0))
+    func roundToNearestN(_ number: Int, n: Int = 1) -> Int {
+        if(n == 1) {
+            return number
+        }
+        return n * Int(round(Double(number) / Double(n)))
     }
     
     // Determine what color the arm should be based on the
@@ -79,12 +82,19 @@ struct ContentView: View {
     // Get the sensor angles by sending an http request to the
     // microcontroller.
     func fetchAngles() async {
+        // When slow/no connection, there is a lot of time between the two
+        // http requests. We use temp variables so that the state upper and
+        // lower arm angles are updated at the same time.
+        var tempUpperArmAngle = 0
+        var tempLowerArmAngle = 0
         // Fetch and convert the upper arm angle
-        upperArmAngle = roundToNearestFive(intToAngle(integer: await fetchAngle(url: lowerArmUrl)))
-        // Find the coordinates of the elbow based on the angle
-        elbowCoords = getLineCoords(startX: Float(shoulderCoords.x), startY: Float(shoulderCoords.y), angle: upperArmAngle, length: 50)
+        tempUpperArmAngle = roundToNearestN(intToAngle(integer: await fetchAngle(url: upperArmUrl)))
         // Fetch and convert the lower arm angle
-        lowerArmAngle = roundToNearestFive(intToAngle(integer: await fetchAngle(url: upperArmUrl)))
+        tempLowerArmAngle = roundToNearestN(intToAngle(integer: await fetchAngle(url: lowerArmUrl)))
+        // Find the coordinates of the elbow based on the angle
+        upperArmAngle = tempUpperArmAngle
+        lowerArmAngle = tempLowerArmAngle
+        elbowCoords = getLineCoords(startX: Float(shoulderCoords.x), startY: Float(shoulderCoords.y), angle: upperArmAngle, length: 50)
         // Find the coordinates of the wrist based on the angle
         wristCoords = getLineCoords(startX: Float(elbowCoords.x), startY: Float(elbowCoords.y), angle: lowerArmAngle, length: 50)
     }
@@ -100,9 +110,31 @@ struct ContentView: View {
                 angle = Int(result) ?? 0
             }
         } catch {
-            angle = 0
+            return 0
         }
         return angle
+    }
+    
+    // Set the warning angle.
+    // If the failure angle is lower, it the same
+    func setWarningAngle(angle: Int) {
+        if(angle >= failureAngle) {
+            failureAngle = angle
+        }
+        warningAngle = angle
+    }
+    
+    // Set the failure angle.
+    // If the warning angle is higher, make it the same
+    func setFaillureAngle(angle: Int) {
+        if(angle <= warningAngle) {
+            warningAngle = angle
+            // If warning angle is now less than 0, make it zero
+            if (warningAngle < 0) {
+                warningAngle = 0
+            }
+        }
+        failureAngle = angle
     }
     
     // Provide two points and the context of a canvas to draw that line on the
@@ -126,37 +158,35 @@ struct ContentView: View {
             Text("Upper arm")
             Menu {
                 Button ("5\u{00B0}") {
-                    warningAngle = 5
+                    setWarningAngle(angle: 5)
                 }
                 Button ("10\u{00B0}") {
-                    warningAngle = 10
+                    setWarningAngle(angle: 10)
                 }
                 Button ("15\u{00B0}") {
-                    warningAngle = 15
+                    setWarningAngle(angle: 15)
+                }
+                Button ("20\u{00B0}") {
+                    setWarningAngle(angle: 20)
                 }
             } label: {
                 Label("Warning angle: \(warningAngle)", systemImage: "chevron.down")
             }
             Menu {
-                if (warningAngle) <= 5 {
-                    Button ("10\u{00B0}") {
-                        failureAngle = 10
-                    }
+                Button ("10\u{00B0}") {
+                    setFaillureAngle(angle: 10)
                 }
-                if (warningAngle) <= 10 {
-                    Button ("15\u{00B0}") {
-                        failureAngle = 15
-                    }
+                Button ("15\u{00B0}") {
+                    setFaillureAngle(angle: 15)
                 }
-                if (warningAngle <= 15) {
-                    Button ("20\u{00B0}") {
-                        failureAngle = 20
-                    }
+                Button ("20\u{00B0}") {
+                    setFaillureAngle(angle: 20)
                 }
-                if (warningAngle <= 20) {
-                    Button ("25\u{00B0}") {
-                        failureAngle = 25
-                    }
+                Button ("25\u{00B0}") {
+                    setFaillureAngle(angle: 25)
+                }
+                Button ("30\u{00B0}") {
+                    setFaillureAngle(angle: 30)
                 }
             } label: {
                 Label("Failure angle: \(failureAngle)", systemImage: "chevron.down")
@@ -173,7 +203,7 @@ struct ContentView: View {
                 let originalWidth: CGFloat = 200;
                 let scale = screenWidth / originalWidth;
                 Canvas { context, size in
-                    // Drwa the head
+                    // Draw the head
                     let circleRect = CGRect(x: 30, y: 50, width: 40, height: 40)
                     context.fill(Path(ellipseIn: circleRect), with: .color(.gray))
                     // Draw the torso
@@ -207,7 +237,7 @@ struct ContentView: View {
         }
         .onAppear() {
             // Set a timer to fetch the angles every 0.1 seconds
-            timer = Timer.publish(every: 0.1, on: .main, in: .common)
+            timer = Timer.publish(every: 2, on: .main, in: .common)
                 .autoconnect()
                 .sink { _ in
                     Task {
